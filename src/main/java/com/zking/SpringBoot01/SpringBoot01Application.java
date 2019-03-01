7aa1fc67b71436871ea5fccdf5970d2db3b42272
usernamewyf <2339294680@qq.com>
2019-03-01 11:36:51 +0800

    first commit

---
+++ src/main/java/com/zking/SpringBoot01/SpringBoot01Application.java
@@ -0,0 +1,14 @@
+package com.zking.SpringBoot01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot01Application.class, args);
+	}
+
+}
+
